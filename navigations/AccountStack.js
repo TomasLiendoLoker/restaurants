4784a1cc74d98b427435bae25a714b1068e441e8
Tomas Liendo Loker <TomasLLoker@gmail.com>
2022-03-10 15:03:34 -0300

    Se agrega registrar usuario
    Se agrega el formulario correspondiente al registro del usuario

--- navigations/AccountStack.js
+++ navigations/AccountStack.js
@@ -1,26 +1,31 @@
-
-import { createStackNavigator } from '@react-navigation/stack'
-import React from 'react'
-import Account from '../screens/account/Account'
-import Login from '../screens/account/Login'
+import { createStackNavigator } from "@react-navigation/stack";
+import React from "react";
+import Account from "../screens/account/Account";
+import Login from "../screens/account/Login";
+import Register from "../screens/account/Register";
 //Creamos un stack de navegacion
-const Stack = createStackNavigator()
+const Stack = createStackNavigator();
 
 export default function AccountStack() {
   return (
-   //cremos una pantalla por stack de navegacion
-   //Agregamos para navegar a otra pantalla como login
-   <Stack.Navigator>
-       <Stack.Screen
-       name="account"
-       component={Account}
-       options={{title: "Cuenta"}}
-       />
-        <Stack.Screen
-       name="login"
-       component={Login}
-       options={{title: "Iniciar Sesion"}}
-       />
-   </Stack.Navigator>
-  )
+    //cremos una pantalla por stack de navegacion
+    //Agregamos para navegar a otra pantalla como login
+    <Stack.Navigator>
+      <Stack.Screen
+        name="account"
+        component={Account}
+        options={{ title: "Cuenta" }}
+      />
+      <Stack.Screen
+        name="login"
+        component={Login}
+        options={{ title: "Iniciar Sesion" }}
+      />
+      <Stack.Screen
+        name="register"
+        component={Register}
+        options={{ title: "Registrar Usuario" }}
+      />
+    </Stack.Navigator>
+  );
 }
